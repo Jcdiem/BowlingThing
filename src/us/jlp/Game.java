63ecd4d2d3a760f38224c408fee3345129f98a00
jcdiem <jcdiem3@gmail.com>
2019-10-08 20:13:45 -0500

    Initial commit

---
+++ src/us/jlp/Game.java
@@ -0,0 +1,11 @@
+package us.jlp;
+
+/**
+ * Controls and runs every aspect of the program
+ */
+public class Game {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
