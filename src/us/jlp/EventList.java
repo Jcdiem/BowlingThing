f766393ac8c719200f5f4d84f464a64d38482728
Jake Mierow <jakee.m@icloud.com>
2019-10-08 20:59:37 -0500

    updated classes

--- src/us/jlp/EventList.java
+++ src/us/jlp/EventList.java
@@ -2,6 +2,24 @@ package us.jlp;
 
 public class EventList
 {
-    
+
+    public static void doOpen()
+    {
+
+    }
+
+    public static void doStrike()
+    {
+
+    }
+
+    public static void doSpare()
+    {
+
+    }
+
+
+
+
 }
 
--- src/us/jlp/Player.java
+++ src/us/jlp/Player.java
@@ -2,5 +2,20 @@ package us.jlp;
 
 public class Player
 {
+    new Set Set();
+
+}
+
+public int bowl(int eventNum)
+{
+
+    switch(eventNum)
+    case 1: EventList.doOpen();
+    break;
+    case 2: EventList.doStrike();
+    break;
+    case 3: EventList.doSpare();
+    break;
+
 
 }
